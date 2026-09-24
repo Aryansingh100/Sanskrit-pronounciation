ffa3e48b479b7ee37c5281ca17e2c1312fe0b81d
Aryan <0310aryan@gmail.com>
2026-09-24 15:06:21 -0400

    Implementing extra recognition

--- sanskrit-pronunciation/src/App.jsx
+++ sanskrit-pronunciation/src/App.jsx
@@ -84,10 +84,6 @@ const initialWords = [
       "भवतो नाम किं",
       "भवतो नाम किम्",
       "भगत नाम किम",
-      "बहुत नाम के",
-      "भगत नाम की",
-      "बहुत नमकम",
-      "भगत नाम के",
       "bhavatah nama kim",
       "bhavataha nama kim",
       "bhavato nama kim"
@@ -113,6 +109,34 @@ const generateIAST = async (sanskritText) => {
   const result = await response.text();
 
   return result.trim();
+};
+
+const calculateEditDistance = (a, b) => {
+  const matrix = [];
+
+  for (let i = 0; i <= b.length; i++) {
+    matrix[i] = [i];
+  }
+
+  for (let j = 0; j <= a.length; j++) {
+    matrix[0][j] = j;
+  }
+
+  for (let i = 1; i <= b.length; i++) {
+    for (let j = 1; j <= a.length; j++) {
+      if (b.charAt(i - 1) === a.charAt(j - 1)) {
+        matrix[i][j] = matrix[i - 1][j - 1];
+      } else {
+        matrix[i][j] = Math.min(
+            matrix[i - 1][j] + 1,
+            matrix[i][j - 1] + 1,
+            matrix[i - 1][j - 1] + 1
+        );
+      }
+    }
+  }
+
+  return matrix[b.length][a.length];
 };
 
 function App() {
